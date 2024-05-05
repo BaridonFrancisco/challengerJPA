5598c057d7648736bc79e77060baa3f3a740875c
BaridonFrancisco <baridonfrancisco@gmail.com>
2024-05-05 20:25:58 -0300

    configuracion inicial

---
+++ src/main/java/com/jpaChallenger/JpaChallenger/JpaChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.jpaChallenger.JpaChallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpaChallenger/JpaChallenger/JpaChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpaChallenger.JpaChallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
